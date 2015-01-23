0181ecb049abc40097185a533a3f99165d88c892
vaszol <vaszol@mail.ru>
2015-01-23 13:03:23 +0300

    анимация элемента из контекстного меню

---
+++ app/src/androidTest/java/ru/vaszol/vas/p0201_sampleanimation/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.vaszol.vas.p0201_sampleanimation;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
